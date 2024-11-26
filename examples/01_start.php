23e1d6a35d71a91eff29982b86a75850dfd3a6cd
Paulo <32718867+paulocezargit@users.noreply.github.com>
2024-11-26 09:53:21 -0300

    Defining the Style Sheet
    This time, we started creating our CSS style sheet, which will be used in the project. We defined the size and resolution of the questionnaire elements, ensuring they fit properly on the website and are responsive.

--- examples/01_start.php
+++ examples/01_start.php
@@ -1,13 +1,15 @@
+<?php include("../questphp.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>01_start</title>
+	<link rel="stylesheet" type="text/css" href="../questphp.css">
 </head>
 <body>
 
-<?php include("./questphp.php"); ?>
 
+    <script src="../questphp.js"></script>
 </body>
 </html>
